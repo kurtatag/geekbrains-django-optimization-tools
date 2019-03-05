b984526879c75f87d6920b986fa61951035764d3
Khetag Tebiev <khetag_tebiev@epam.com>
2019-03-06 00:36:54 +0300

    Add ordersapp/forms.py

---
+++ ordersapp/forms.py
@@ -0,0 +1,26 @@
+from django.forms import ModelForm
+from ordersapp.models import Order, OrderItem
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+class OrderItemForm(ModelForm):
+    class Meta:
+        model = OrderItem
+        fields = ()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
