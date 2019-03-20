88cffc5fab8bb96cf11d2ea1decfcbfcb9b99cfe
Khetag Tebiev <khetag_tebiev@epam.com>
2019-03-20 06:13:21 +0300

    Task-05: Apply cache to view with ajax

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 import mainapp.views as mainapp_views
 
@@ -7,6 +8,6 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp_views.products, name='index'),
     path('<int:product_id>/', mainapp_views.product_details, name='product_details'),
-    path('<str:current_product_category>/', mainapp_views.products, name='category'),
+    path('<str:current_product_category>/', cache_page(3600)(mainapp_views.products_ajax), name='category'),
     path('price/<int:product_id>/json/', mainapp_views.ProductPrice.as_view(), name='product_price'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic.detail import View
 from django.conf import settings
 from django.core.cache import cache
+from django.template.loader import render_to_string
 
 from .models import Product, ProductCategory
 
@@ -71,6 +72,54 @@ def products(request: HttpRequest, current_product_category='all'):
     }
 
     return render(request, 'mainapp/products.html', context)
+
+
+def products_ajax(request: HttpRequest, current_product_category='all'):
+
+    # get managers for products & categories
+    # categories = ProductCategory.objects
+    products = Product.objects
+
+    # prepare a list of categories for "product category menu"
+    product_category_list = ['all'] + [c.name for c in get_product_category_list()]
+
+    # prepare a list of products
+    if current_product_category == 'all':
+        product_list = products.filter(
+            is_active=True,
+            category__is_active=True
+        )
+    else:
+        product_list = products.filter(
+            is_active=True,
+            category__name=current_product_category
+        )
+
+    # prepare products paginator
+    page = 1
+    if 'page' in request.GET:
+        page = request.GET['page']
+    paginator = Paginator(product_list, 2)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    # prepare the context for the template
+    context = {
+        'product_category_list': product_category_list,
+        'current_product_category': current_product_category,
+        'product_list': products_paginator,
+    }
+
+    result = render_to_string(
+        'mainapp/includes/inc_products_list.html',
+        context=context,
+        request=request)
+
+    return JsonResponse({ 'result' : result})
 
 
 def product_details(request: HttpRequest, product_id):
